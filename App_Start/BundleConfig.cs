b7067aa11bc1144f6271d647aea843b70ba52c0a
Anderson Pereira <anderson.rasec@outlook.com>
2016-12-15 01:19:32 -0300

    Resolve the correct name of the reference Toastr
    This commit revolve the correct name of the toastr.js in the project.

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -10,7 +10,7 @@ namespace DemoCRUD
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/toast.js*"));
+                        "~/Scripts/toastr.js*"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
