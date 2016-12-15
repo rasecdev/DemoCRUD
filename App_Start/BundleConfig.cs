1325e088009d91b0509d6534f8180c80ce2d02be
Anderson Pereira <anderson.rasec@outlook.com>
2016-12-15 01:13:16 -0300

    Install Toast
    This commit install Toast.js for the project.

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace DemoCRUD
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/toast.js*"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
